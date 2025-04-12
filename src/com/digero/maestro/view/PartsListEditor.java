ceb3e531c04da6d686d61745f5d9203a6624aa99
Nikolai V. Chr. <emptyyetfull@gmail.com>
2025-04-12 19:55:28 +0200

    Some part listener changes

--- src/com/digero/maestro/view/PartsListEditor.java
+++ src/com/digero/maestro/view/PartsListEditor.java
@@ -4,18 +4,46 @@ import java.awt.Dimension;
 import java.util.ArrayList;
 
 import com.digero.common.midi.SequencerWrapper;
+import com.digero.common.util.Listener;
 import com.digero.maestro.abc.AbcPart;
+import com.digero.maestro.abc.AbcSong;
+import com.digero.maestro.abc.AbcSongEvent;
 
 public class PartsListEditor extends PartsList {
 	private static final long serialVersionUID = -3564677504833477636L;
+	Dimension rowDim = null;
 
 	public PartsListEditor(SequencerWrapper abcSequencer, MiscSettings miscSettings) {
 		super(abcSequencer, miscSettings);
+		songListener = e -> {
+			AbcSong song = e.getSource();
+			if (song == null)
+				return;
+
+			switch (e.getProperty()) {
+			case PART_ADDED:
+				e.getPart().addAbcListener(partListener);
+				//updateParts(); listens to model also, so not needed
+				break;
+			case BEFORE_PART_REMOVED:
+				AbcPart part = e.getPart();
+				part.removeAbcListener(partListener);
+				break;
+			case PART_LIST_ORDER:
+				updateParts();
+				break;
+			default:
+				break;
+			}
+		};
+		itemListener = null;
 	}
 	
 	@Override
 	public Dimension getPreferredSize() {
-		Dimension rowDim = PartEditorItem.getProtoDimension();
+		if (rowDim == null) {
+			rowDim = PartEditorItem.getProtoDimension();
+		}
 		rowDim.height = getComponentCount() * rowDim.height;
 		return rowDim;
 	}
