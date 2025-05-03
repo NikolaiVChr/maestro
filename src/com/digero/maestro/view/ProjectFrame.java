81e0b60a573cd6e74ed6abc271a8e65ee4065e98
Nikolai V. Chr. <emptyyetfull@gmail.com>
2025-05-03 20:02:19 +0200

    Async version check and now gives chance to close project. Versioncheck can also be run multiple times now if needed.

--- src/com/digero/maestro/view/ProjectFrame.java
+++ src/com/digero/maestro/view/ProjectFrame.java
@@ -34,12 +34,14 @@ import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.prefs.Preferences;
 
 import javax.imageio.ImageIO;
@@ -261,7 +263,8 @@ public class ProjectFrame extends JFrame implements TableLayoutConstants, ICompi
 	
 	private AudioExportManager audioExporter;
 
-	
+	private volatile Version latestVer;
+	private CompletableFuture<Void> future = null;
 	
 	/*
 	 * private static Color BRIGHT_RED = new Color(255, 0, 0); private static Color ORANGE = new Color(235, 150, 64);
@@ -1990,6 +1993,10 @@ public class ProjectFrame extends JFrame implements TableLayoutConstants, ICompi
 		return (Integer) tempoSpinner.getValue();
 	}
 
+	/**
+	 * 
+	 * @return true if it was closed
+	 */
 	public boolean closeSong() {
 		SectionEditor.clearClipboard();
 		TrackPanel.clearDrumClipboard();
@@ -2895,34 +2902,53 @@ public class ProjectFrame extends JFrame implements TableLayoutConstants, ICompi
 	}
 	
 	private void checkVersionCompare() {
-		try {
-			String fileUrl = "https://raw.githubusercontent.com/NikolaiVChr/mver/refs/heads/main/main";
-			URI uri = new URI(fileUrl);
-			URL url = uri.toURL();
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setConnectTimeout(2000);
-			connection.setReadTimeout(3000);
-			connection.setRequestMethod("GET");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				Version latestVer = Version.parseVersion(line);
-				Version myVersion = MaestroMain.APP_VERSION;
-				if (latestVer != null && myVersion.compareTo(latestVer) < 0) {
-					int result = JOptionPane.showConfirmDialog(ProjectFrame.this, "Version "+latestVer+" is available, do you want to download it?", "Version check",
-							JOptionPane.YES_NO_OPTION);
-					if (result == JOptionPane.YES_OPTION) {
-						URI uriDownload = new URI("https://drive.google.com/drive/folders/1CigT_AloFP34lZbIEvb4CsqGmBL8vodu");
-						if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-							Desktop.getDesktop().browse(uriDownload);
-							System.exit(0);
+		if(future == null || future.isDone()) {
+			future = CompletableFuture.runAsync(() -> {
+				try {
+					String fileUrl = "https://raw.githubusercontent.com/NikolaiVChr/mver/refs/heads/main/main";
+					URI uri = new URI(fileUrl);
+					URL url = uri.toURL();
+					HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+					connection.setConnectTimeout(4000);
+					connection.setReadTimeout(6000);
+					connection.setRequestMethod("GET");
+					try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+						String line;
+						while ((line = reader.readLine()) != null) {
+							latestVer = Version.parseVersion(line);
+							Version myVersion = MaestroMain.APP_VERSION;
+							if (latestVer != null && myVersion.compareTo(latestVer) < 0) {
+								SwingUtilities.invokeLater(() -> {
+									int result = JOptionPane.showConfirmDialog(ProjectFrame.this, "Version "+latestVer+" is available, do you want to close and download it?", "Version check",
+											JOptionPane.YES_NO_OPTION);
+										if (result == JOptionPane.YES_OPTION) {
+											URI uriDownload;
+											try {
+												uriDownload = new URI("https://drive.google.com/drive/folders/1CigT_AloFP34lZbIEvb4CsqGmBL8vodu");
+											
+												if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+													if (closeSong()) {
+														Desktop.getDesktop().browse(uriDownload);
+														System.exit(0);
+													}
+												}
+											} catch (URISyntaxException | IOException e) {
+												e.printStackTrace();
+											}
+										}
+									}
+								);
+								break;
+							}
 						}
+					} catch (Exception io) {
+						io.printStackTrace();
 					}
+					connection.disconnect();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			}
-			reader.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+			});
 		}
 	}
 }
