3a7f27340a62d02a0b232acac99c4ac8931cc2ed
Nikolai V. Chr. <emptyyetfull@gmail.com>
2025-08-16 10:32:49 +0200

    Removed some commented code.

--- src/com/digero/maestro/view/ProjectFrame.java
+++ src/com/digero/maestro/view/ProjectFrame.java
@@ -1397,7 +1397,6 @@ public class ProjectFrame extends JFrame implements TableLayoutConstants, ICompi
 				if (dialog.isNumbererSettingsChanged()) {
 					partAutoNumberer.setSettings(dialog.getNumbererSettings());
 					partAutoNumberer.renumberAllParts();
-                    //partsList.updateParts();
 				}
 				partNameTemplate.setSettings(dialog.getNameTemplateSettings());
 				partPanel.settingsChanged();
