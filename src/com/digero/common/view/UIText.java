bd00720b41e6392b0014483d6e4ffb85529a468f
Nikolai V. Chr. <emptyyetfull@gmail.com>
2025-12-29 15:18:01 +0100

    Make project resources folder structure more like Maven and IntelliJ expect. Next step sometimes in 2026 is to make src folder structure Maven default also: -src  -main  -resources  -test The resource uitext bundle keys are horrible, but even more is going through 900 of them manually, hence the long keys. 1800 localizations added, compliment of Gemini 3 Pro. Be aware, that many keys have trailing and/or leading whitespace, so edit carefully.

---
+++ src/com/digero/common/view/UIText.java
@@ -0,0 +1,27 @@
+package com.digero.common.view;
+
+import com.digero.maestro.MaestroMain;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+
+public class UIText {
+    private static final Logger log = Logger.getLogger("locale"); //NON-NLS
+    private static final String locale = Preferences.userNodeForPackage(MaestroMain.class).node("miscSettings").get("locale", null); //NON-NLS
+    private static final @NonNls String BUNDLE_NAME = "uitext";
+    private static final ResourceBundle uiText = ResourceBundle.getBundle(BUNDLE_NAME, locale==null? Locale.getDefault():Locale.of(locale)); //NON-NLS
+
+    public static @NotNull String get(@PropertyKey(resourceBundle = BUNDLE_NAME)String key) {
+        try {
+            return uiText.getString(key);
+        } catch (Exception e) {
+            log.warning("Failed to load UI text for key \"" + key + "\", locale is " + locale); //NON-NLS //NON-NLS
+            return "ERROR";
+        }
+    }
+}
