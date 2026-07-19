9044969ab6f008e4cb563248b66aec5fd290d558
SapphireFlame96 <80835556+BinaryBaggins@users.noreply.github.com>
2026-07-19 15:47:34 +0200

    Add Locale and Double converters to DefaultConverters for enhanced settings serialization

--- src/com/digero/common/settings/converter/DefaultConverters.java
+++ src/com/digero/common/settings/converter/DefaultConverters.java
@@ -1,5 +1,7 @@
 package com.digero.common.settings.converter;
 
+import java.util.Locale;
+
 import com.digero.common.settings.SettingConverter;
 
 public final class DefaultConverters {
@@ -37,6 +39,26 @@ public final class DefaultConverters {
         @Override
         public Boolean deserialize(String value) {
             return Boolean.parseBoolean(value);
+        }
+    };
+
+    public static final SettingConverter<Double> DOUBLE = new SettingConverter<>() {
+        public Double deserialize(String value) {
+            return Double.valueOf(value);
+        }
+
+        public String serialize(Double value) {
+            return String.valueOf(value);
+        }
+    };
+
+    public static final SettingConverter<Locale> LOCALE = new SettingConverter<>() {
+        public Locale deserialize(String value) {
+            return Locale.forLanguageTag(value);
+        }
+
+        public String serialize(Locale value) {
+            return value.toLanguageTag();
         }
     };
 
